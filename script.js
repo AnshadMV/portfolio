c857a6ba918a2bc8182be83cf52228c22e29acd5
anshadvelladath <anshadvelladath@gmail.com>
2025-08-25 15:26:16 +0530

    second

--- script.js
+++ script.js
@@ -67,3 +67,35 @@ document.addEventListener('DOMContentLoaded', function() {
     
     sections.forEach(section => observer.observe(section));
 });
+
+
+
+
+
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("fname").value,
+    lname: document.getElementById("lname").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    subject: document.getElementById("title").value,
+  };
+
+  const serviceID = "service_kjbvx5e";
+  const templateID = "template_bytqbde";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("fname").value = "";
+        document.getElementById("lname").value="";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        document.getElementById("title").value="";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
